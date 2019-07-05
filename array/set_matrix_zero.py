d6c20ae240e4617ad2dee9f8ba5e272c098f6bdc
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-07-05 10:30:46 +0900

    matrix zeros

---
+++ array/set_matrix_zero.py
@@ -0,0 +1,40 @@
+class Solution(object):
+  def setZeroes(self, matrix):
+      # First row has zero?
+      m, n, firstRowHasZero = len(matrix), len(matrix[0]), not all(matrix[0])
+      # Use first row/column as marker, scan the matrix
+      for i in range(1, m):
+          for j in range(n):
+              if matrix[i][j] == 0:
+                  matrix[0][j] = matrix[i][0] = 0
+      # Set the zeros
+      for i in range(1, m):
+          for j in range(n - 1, -1, -1):
+              if matrix[i][0] == 0 or matrix[0][j] == 0:
+                  matrix[i][j] = 0
+      # Set the zeros for the first row
+      if firstRowHasZero:
+          matrix[0] = [0] * n
+
+
+
+s = Solution()
+matrix = [
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+s.setZeroes(matrix)
+for x in range(len(matrix)):
+    print(matrix[x])
+
+print("")
+
+matrix = [
+  [0,1,2,0],
+  [3,4,5,2],
+  [1,3,1,5]
+]
+s.setZeroes(matrix)
+for x in range(len(matrix)):
+    print(matrix[x])
