6ad73a88065a9ccd333364aec3f6f4828553ebc2
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-08-20 22:05:46 +0900

    abc138d

--- atcoder/abc138/d.py
+++ atcoder/abc138/d.py
@@ -1,24 +1,25 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-N, Q = list(map(int, input().split()))
+readline = sys.stdin.readline
+n, q = list(map(int, readline().split()))
 # 双方向に張る
-tree = [[] for _ in range(N)]
-for i in range(N-1):
-    a, b = list(map(int, input().split()))
+tree = [[] for _ in range(n)]
+for i in range(n-1):
+    a, b = list(map(int, readline().split()))
     a -= 1
     b -= 1
     tree[a].append(b)
     tree[b].append(a)
 
 # queryのスコア表を作る
-total = [0 for _ in range(N)]
-for i in range(Q):
-    p, x = list(map(int, input().split()))
+total = [0 for _ in range(n)]
+for i in range(q):
+    p, x = list(map(int, readline().split()))
     total[p - 1] += x
 
 # dfs
-ans = [0 for _ in range(N)]
+ans = [0 for _ in range(n)]
 def dfs(v, p, value):
     value += total[v]
     ans[v] = value
@@ -27,4 +28,4 @@ def dfs(v, p, value):
         dfs(c, v, value)
 
 dfs(0, -1, 0)
-print(" ".join(list(map(str, ans))))
+print(*ans)
