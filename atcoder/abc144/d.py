a6b38bac4ca256260a6e919964b0a9231543229f
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-10-28 09:56:15 +0900

    abc144d

--- atcoder/abc144/d.py
+++ atcoder/abc144/d.py
@@ -1,7 +1,11 @@
 # 死んだ
 import math
 a, b, x = list(map(int, input().split()))
-# c = x / (a*a)
 
-# d = (b - c) * 2
-# ad = math.sqrt(d*d + a*a)
+ans = 0
+if a * a *b / 2.0 <= x:
+    ans = math.atan(2 * (a * a * b - x) / (a * a * a))
+else:
+    ans = math.pi / 2 - math.atan(2 * x / (a * b * b))
+
+print(math.degrees(ans))
