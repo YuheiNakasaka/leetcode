a5049835666c217fa941189b72ff3842b9a545d1
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-12-06 10:12:47 +0900

    abc099c

---
+++ atcoder/abc099_c.py
@@ -0,0 +1,14 @@
+N = int(input())
+res = N
+for i in range(N+1):
+    cc = 0
+    t = i
+    while t > 0:
+        cc += t % 6
+        t //= 6
+    t = N - i
+    while t > 0:
+        cc += t % 9
+        t //= 9
+    res = min(res, cc)
+print(int(res))
